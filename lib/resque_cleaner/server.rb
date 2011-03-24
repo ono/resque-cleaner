43797d509e9b6b8fc55908a599a3bff11ff4dff0
Tatsuya Ono <ononoma@gmail.com>
2011-03-24 22:40:28 +0000

    show failed grouped by class

--- lib/resque_cleaner/server.rb
+++ lib/resque_cleaner/server.rb
@@ -5,18 +5,34 @@ module ResqueCleaner
     def self.erb_path(filename)
       File.join(File.dirname(__FILE__), 'server', 'views', filename)
     end
+    def self.public_path(filename)
+      File.join(File.dirname(__FILE__), 'server', 'public', filename)
+    end
 
     def self.included(base)
 
       base.class_eval do
         helpers do
+
         end
 
         get "/cleaner" do
+          @stats = cleaner.stats_by_class
+
           erb File.read(ResqueCleaner::Server.erb_path('cleaner.erb'))
+        end
+
+        get /cleaner\/public\/([a-z]+\.[a-z]+)/ do
+          send_file ResqueCleaner::Server.public_path(params[:captures].first)
         end
       end
 
+    end
+
+    def cleaner
+      @cleaner ||= Resque::Plugins::ResqueCleaner.new
+      @cleaner.print_message = false
+      @cleaner
     end
     Resque::Server.tabs << 'Cleaner'
   end
