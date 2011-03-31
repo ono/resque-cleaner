78483d6c6a0342ccea47b02eebfe8d1f1b16fdcc
Tatsuya Ono <ononoma@gmail.com>
2011-03-31 22:57:55 +0100

    pagination

--- lib/resque_cleaner/server.rb
+++ lib/resque_cleaner/server.rb
@@ -9,6 +9,56 @@ module ResqueCleaner
       File.join(File.dirname(__FILE__), 'server', 'public', filename)
     end
 
+    # Pagination helpr for list page.
+    class Paginate
+      attr_accessor :page_size, :page, :jobs, :url
+      def initialize(jobs, url, page=1, page_size=20)
+        @jobs = jobs
+        @url = url
+        @page = (!page || page < 1) ? 1 : page
+        @page_size = 20
+      end
+
+      def first_index
+        @page_size * (@page-1)
+      end
+
+      def last_index
+        last = first_index + @page_size - 1
+        last > @jobs.size-1 ? @jobs.size-1 : last
+      end
+
+      def paginated_jobs
+        @jobs[first_index,@page_size]
+      end
+
+      def first_page?
+        @page <= 1
+      end
+
+      def last_page?
+        @page >= max_page
+      end
+
+      def page_url(page)
+        u = @url
+        u += @url.include?("?") ? "&" : "?"
+        if page.is_a?(Symbol)
+          page = @page - 1 if page==:prev
+          page = @page + 1 if page==:next
+        end
+        u += "p=#{page}"
+      end
+
+      def total_size
+        @jobs.size
+      end
+
+      def max_page
+        ((total_size-1) / @page_size) + 1
+      end
+    end
+
     def self.included(base)
 
       base.class_eval do
@@ -71,6 +121,9 @@ module ResqueCleaner
           }
 
           @failed = cleaner.select(&block).reverse
+
+          url = "cleaner_list?c=#{@klass}&f=#{@from}&t=#{@to}"
+          @paginate = Paginate.new(@failed, url, params[:p].to_i)
 
           @klasses = cleaner.stats_by_class.keys
           @count = cleaner.select(&block).size
