b9899e87de67f73ab51994f11e7e4768d7762305
Tatsuya Ono <ononoma@gmail.com>
2011-04-05 22:03:35 +0100

    test for resque-web.

---
+++ test/resque_web_test.rb
@@ -0,0 +1,44 @@
+require File.dirname(__FILE__) + '/test_helper'
+
+# Pull in the server test_helper from resque
+require 'resque/server/test_helper.rb'
+
+def setup_some_failed_jobs
+  Resque.redis.flushall
+
+  @worker = Resque::Worker.new(:jobs,:jobs2)
+
+  10.times {|i|
+    create_and_process_jobs :jobs, @worker, 1, Time.now, BadJob, "test_#{i}"
+  }
+
+  @cleaner = Resque::Plugins::ResqueCleaner.new
+  @cleaner.print_message = false
+end
+
+context "resque-web" do
+  setup do
+    setup_some_failed_jobs
+  end
+
+  test "#cleaner should respond with success" do
+    get "/cleaner_list"
+    assert last_response.ok?, last_response.errors
+  end
+
+  test "#cleaner_list should respond with success" do
+    get "/cleaner_list"
+    assert last_response.ok?, last_response.errors
+  end
+
+  test '#cleaner_list shows the failed jobs' do
+    get "/cleaner_list"
+    assert last_response.body.include?('BadJob')
+  end
+
+  test '#cleaner_exec clears job' do
+    post "/cleaner_exec", :action => "clear", :sha1 => Digest::SHA1.hexdigest(@cleaner.select[0].to_json)
+    assert_equal 9, @cleaner.select.size
+  end
+end
+
