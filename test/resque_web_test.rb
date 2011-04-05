8df3afd8fd05fcdc128708185090fc6e5bc6c579
Tatsuya Ono <ononoma@gmail.com>
2011-04-05 22:16:27 +0100

    tweak for ruby 1.9.2

--- test/resque_web_test.rb
+++ test/resque_web_test.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/test_helper'
+require File.expand_path(File.dirname(__FILE__) + '/test_helper')
 
 # Pull in the server test_helper from resque
 require 'resque/server/test_helper.rb'
